d261a5067586fe06593c317bb2072055a9e5730e
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-09-02 15:03:44 +0200

    todo list funzionante

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,48 @@
-console.log('ciao');
-console.log(Vue);
 
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            //creo il mio array di oggeti con all'interno di esso text e done cioè descrizione e se è stata già fatta come task.
+            todo: [
+                {
+                    text: 'prepararsi',
+                    done: true
+                },
+                {
+                    text: 'fare colazione',
+                    done: false
+                },
+                {
+                    text: 'ascoltare la lezione',
+                    done: true
+                },
+                {
+                    text: 'esercitarsi',
+                    done: false
+                },
+            ],
+            //creo una variabile dove poter poi inserire dinamicamente tramite input HTMl altri oggetti con le chiavi text e done dentro il mio Array
+            newTodo: '',
+        }
+    },
+    methods: {
+        //creo una funzione che mi permette di aggiungere elementi alla mia lista di cose da fare 
+        addTodo() {
+            const cleanTodo = this.newTodo.trim();
+            if(cleanTodo != ''){
+                this.todo.push({
+                    text: cleanTodo, 
+                    done: false,
+                });
+                this.newTodo = '';
+            }
+
+        },
+        //creo una funzione per rimuovere elementi dalla mia lista
+        removeTodo(index) {
+            this.todo.splice(index, 1);
+        }
+    }
+}).mount('#app')
